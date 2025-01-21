f1c42a6073e38535e2127106acb72355d5922f8f
Bryan Yan <bryan.yan@mail.utoronto.ca>
2025-01-21 15:50:06 -0500

    file transfer lab first commit

---
+++ file-transfer-1/deliver.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>     
+#include <string.h>     
+#include <netdb.h>     
+#include <arpa/inet.h> 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define MAXBUFLEN 1500
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: deliver <server address> <server port number>\n");
+    }
+
+    // POPULATE ADDRINFOS
+    int status;
+    struct addrinfo hints;
+    struct addrinfo *servinfo; // linked list of struct addrinfos
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+
+    if ((status = getaddrinfo(argv[1], argv[2], &hints, &servinfo)) != 0) { // non-zero return means error
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+
+    // LOOP THRU AND GET FIRST SOCKET WE CAN
+    int sockfd;
+    struct addrinfo *curr;
+    for (curr = servinfo; curr != NULL; curr = curr->ai_next) {
+        // get socket descriptor
+        sockfd = socket(curr->ai_family, curr->ai_socktype, curr->ai_protocol);
+        if (sockfd == -1) { // if error, try the next addrinfo
+            perror("socket");
+            continue; 
+        }
+        // no need to bind, client can use any random available port
+        break; // if successful for one of the addrinfos, we're good break
+    }
+
+    if (curr == NULL) {
+        fprintf(stderr, "client failed to bind socket\n");
+        exit(1);
+    }
+
+    // // QUERY USER
+    // printf("Input file transfer cmd: ftp <file-name>\n");
+    // char input_buf[MAXBUFLEN] = {0};
+    // if (fgets(input_buf, MAXBUFLEN, stdin) == NULL) {
+    //     printf("Error reading input.\n");
+    //     freeaddrinfo(servinfo);
+    //     close(sockfd);
+    //     exit(1);
+    // }
+    // input_buf[strcspn(input_buf, "\n")] = '\0'; // replace the trailing newline with null character
+
+
+    // char *cmd = strtok(input_buf, " "); // up to first space
+    // char *filename = strtok(NULL, " "); // up to 2nd space (should be end of string)
+    // if (!cmd || !filename || (strtok(NULL, " ") != NULL)) { // make sure cmd and filename are non-NULL and there is nothing left in the input
+    //     printf("Input must be of form ftp <file-name>, with no spaces in the file name.\n");
+    //     freeaddrinfo(servinfo);
+    //     close(sockfd);
+    //     exit(1);
+    // }
+
+    // SEND A MESSAGE
+    char *msg = "ftp";
+    int numbytes;
+    numbytes = sendto(sockfd, msg, strlen(msg), 0, curr->ai_addr, curr->ai_addrlen);
+    if (numbytes == -1) {
+        perror("sendto");
+        freeaddrinfo(servinfo);
+        close(sockfd);
+        exit(1);
+    }
+
+    printf(">>> sent message %d bytes long\n", numbytes);
+
+    // RECEIVE SERVER REPLY
+    char recv_buf[MAXBUFLEN];
+    struct sockaddr_storage serv_addr;
+    socklen_t serv_addr_len = sizeof serv_addr;
+    numbytes = recvfrom(sockfd, recv_buf, MAXBUFLEN - 1, 0, (struct sockaddr *) &serv_addr, &serv_addr_len);
+    if (numbytes == -1) {
+        perror("recvfrom");
+        exit(1);
+    }
+
+    if (strcmp("yes", recv_buf) == 0) {
+        printf("A file transfer can start.\n");
+    } else {
+        freeaddrinfo(servinfo);
+        close(sockfd);
+        exit(1);
+    }
+
+    // don't need it anymore
+    freeaddrinfo(servinfo);
+    close(sockfd);
+
+    return 0;
+}
---
+++ file-transfer-1/server.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>     
+#include <string.h>     
+#include <netdb.h>     
+#include <arpa/inet.h> 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+
+#define MAXBUFLEN 1500
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: server <server port number>\n");
+    }
+
+    // POPULATE ADDRINFOS
+    int status;
+    struct addrinfo hints;
+    struct addrinfo *servinfo; // point to head of linked list of struct addrinfos
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET; // force ipv4
+    hints.ai_socktype = SOCK_DGRAM; // use UDP
+    hints.ai_flags = AI_PASSIVE; // i believe ends up listening on wildcard address, i.e., all interfaces (bc hostname/ipaddr specify as NULL)
+
+    if ((status = getaddrinfo(NULL, argv[1], &hints, &servinfo)) != 0) { // non-zero return means error
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+
+    // LOOP THRU AND BIND TO GET SOCKET FOR FIRST ONE WE CAN
+    int sockfd;
+    struct addrinfo *curr;
+    for (curr = servinfo; curr != NULL; curr = curr->ai_next) {
+        // get socket descriptor
+        sockfd = socket(curr->ai_family, curr->ai_socktype, curr->ai_protocol);
+        if (sockfd == -1) { // if error, try the next addrinfo
+            perror("socket");
+            continue; 
+        }
+        // bind socket descriptor to port
+        if (bind(sockfd, curr->ai_addr, curr->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("bind");
+            continue; 
+        }
+        break; // if successful for one of the addrinfos, we're good break
+    }
+
+    if (curr == NULL) {
+        fprintf(stderr, "server failed to bind socket\n");
+        exit(1);
+    }
+
+    // don't need it anymore
+    freeaddrinfo(servinfo);
+
+    // START ACCEPTING DATA 
+    printf(">>> begin listening...\n");
+
+    while (1) {
+        // receive the message
+        int numbytes;
+        char recv_buf[MAXBUFLEN];
+        struct sockaddr_storage client_addr;
+        socklen_t client_addr_len = sizeof client_addr;
+        numbytes = recvfrom(sockfd, recv_buf, MAXBUFLEN - 1, 0, (struct sockaddr *) &client_addr, &client_addr_len);
+        if (numbytes == -1) {
+            perror("recvfrom");
+            close(sockfd);
+            exit(1);
+        }
+
+        printf(">>> received message %d bytes long\n", numbytes);
+        recv_buf[numbytes] = '\0'; // since we assume it is string
+        printf("%s\n", recv_buf);
+
+        // reply depending on if it's ftp or not
+        if (strcmp("ftp", recv_buf) == 0) {
+            char *send_buf = "yes";
+            printf(">>> replying with yes\n");
+            numbytes = sendto(sockfd, send_buf, strlen(send_buf), 0, (struct sockaddr *) &client_addr, client_addr_len);
+            if (numbytes == -1) {
+                perror("sendto");
+                close(sockfd);
+                exit(1);
+            }
+        } else {
+            char *send_buf = "no";
+            printf(">>> replying with no\n");
+            numbytes = sendto(sockfd, send_buf, strlen(send_buf), 0, (struct sockaddr *) &client_addr, client_addr_len);
+            if (numbytes == -1) {
+                perror("sendto");
+                close(sockfd);
+                exit(1);
+            }
+        }
+    }
+
+    close(sockfd);
+    return 0;
+}
